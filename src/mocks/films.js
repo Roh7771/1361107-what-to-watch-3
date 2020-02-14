8307fbe91db4d1dce9aa7c789a38ecf37425ef50
Belov <belov.rom-77@yandex.ru>
2020-02-14 07:19:13 +0300

    Моки настроены

---
+++ src/mocks/films.js
@@ -0,0 +1,44 @@
+let id = 1;
+
+export default [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    imgSrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    id: id++
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    imgSrc: `img/bohemian-rhapsody.jpg`,
+    id: id++
+  },
+  {
+    title: `Macbeth`,
+    imgSrc: `img/macbeth.jpg`,
+    id: id++
+  },
+  {
+    title: `Aviator`,
+    imgSrc: `img/aviator.jpg`,
+    id: id++
+  },
+  {
+    title: `We need to talk about Kevin`,
+    imgSrc: `img/we-need-to-talk-about-kevin.jpg`,
+    id: id++
+  },
+  {
+    title: `What We Do in the Shadows`,
+    imgSrc: `img/what-we-do-in-the-shadows.jpg`,
+    id: id++
+  },
+  {
+    title: `Revenant`,
+    imgSrc: `img/revenant.jpg`,
+    id: id++
+  },
+  {
+    title: `Johnny English`,
+    imgSrc: `img/johnny-english.jpg`,
+    id: id++
+  },
+];
---
+++ src/mocks/settings.js
@@ -0,0 +1,8 @@
+export default {
+  PROMO_FILM: {
+    promoFilmTitle: `The Grand Budapest Hotel`,
+    promoFilmGenre: `Drama`,
+    promoFilmReleaseYear: 2014
+  },
+};
+
