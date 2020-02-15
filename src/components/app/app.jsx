dfaf232298382506a465356a5bae7a96e5c2a764
Belov <belov.rom-77@yandex.ru>
2020-02-15 11:35:13 +0300

    App-компонент изменен

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,29 +1,58 @@
 import React from "react";
 import Main from "../main/main.jsx";
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 import MoviePage from "../movie-page/movie-page.jsx";
 
-const handlerTitleButtonClick = () => {};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {chosenFilm: null};
+    this._renderApp = this._renderApp.bind(this);
+    this._handlerMovieCardClick = this._handlerMovieCardClick.bind(this);
+  }
 
-const App = ({promoFilm, filmsList}) => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Main
-            filmsList={filmsList}
-            promoFilm={promoFilm}
-            onTitleButtonClick={handlerTitleButtonClick}
-          />
-        </Route>
-        <Route exact path="/dev-movie-page">
-          <MoviePage/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+  _handlerMovieCardClick(chosenFilm) {
+    this.setState({
+      chosenFilm
+    });
+  }
+
+  _renderApp() {
+    const {filmsList, promoFilm} = this.props;
+    const {chosenFilm} = this.state;
+
+    if (chosenFilm) {
+      return (
+        <MoviePage film={chosenFilm} />
+      );
+    }
+
+    if (!chosenFilm) {
+      return (
+        <Main
+          filmsList={filmsList}
+          promoFilm={promoFilm}
+          onMovieCardClick={this._handlerMovieCardClick}
+        />
+      );
+    }
+
+    return <p>Что-то пошло не так :(</p>;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   promoFilm: PropTypes.shape({
@@ -31,11 +60,13 @@ App.propTypes = {
     promoFilmGenre: PropTypes.string,
     promoFilmReleaseYear: PropTypes.number
   }).isRequired,
-  filmsList: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    imgSrc: PropTypes.string,
-    id: PropTypes.id,
-  })).isRequired,
+  filmsList: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        imgSrc: PropTypes.string,
+        id: PropTypes.id
+      })
+  ).isRequired
 };
 
 export default App;
