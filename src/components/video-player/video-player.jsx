69d81d80b815ffdce709b67d524e7852918005cb
FIRST_NAME LAST_NAME <belov.rom-77@yandex.ru>
2020-02-18 18:21:01 +0300

    Добавлен новый компонент с видеоплеером

---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,47 @@
+import React, {PureComponent, createRef} from 'react';
+import PropTypes from 'prop-types';
+
+class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._videoRef = createRef();
+  }
+
+  componentDidMount() {
+    const {videoSrc} = this.props;
+    const video = this._videoRef.current;
+
+    video.src = videoSrc;
+
+    video.ontimeupdate = () => this.setState({
+      progress: video.currentTime
+    });
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.props.isPlaying) {
+      video.play();
+    } else {
+      video.load();
+    }
+  }
+
+  render() {
+    const {posterSrc, videoSrc} = this.props;
+    return (
+      <video ref={this._videoRef} defaultmuted="true" poster={posterSrc} src={videoSrc} alt="" width="280" height="175" />
+    );
+  }
+}
+
+VideoPlayer.propTypes = {
+  videoSrc: PropTypes.string.isRequired,
+  posterSrc: PropTypes.string.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+};
+
+export default VideoPlayer;
+
