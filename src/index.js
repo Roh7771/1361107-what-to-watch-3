2cae2702c51ef62b2e6163cabecf3fefce3a440f
Belov <belov.rom-77@yandex.ru>
2020-03-03 21:01:14 +0300

    thunk и api подключены

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {reducer} from "./reducer.js";
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from "react-redux";
+import {createAPI} from "./api.js";
+import thunk from "redux-thunk";
+
+const api = createAPI(() => {});
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById(`root`));
