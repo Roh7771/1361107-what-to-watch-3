94939a3aa69774e5368766e99f555cda08571680
Belov <belov.rom-77@yandex.ru>
2020-03-13 07:16:55 +0300

    При нажатии на логотип рендерится главная страница

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -30,11 +30,11 @@ const Main = ({
 
         <header className="page-header movie-card__head">
           <div className="logo">
-            <a className="logo__link">
+            <Link to="/" className="logo__link">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
           <div className="user-block">
             {authorizationStatus === AuthorizationStatus.AUTH ? (
